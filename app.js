ebcc4ddbbaade7ee0ac13253351d2211fcd36d00
Tanmay Awasekar <tanmayawasekar@gmail.com>
2020-04-26 16:16:44 +0530

    config.yml and deploy.sh

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.get('/auth/facebook/callback',
   passport.authenticate('facebook',{ scope : ['user_friends', 'public_profile', 'user_friends', 'manage_pages',  'email'] , successRedirect: '/',
                                       failureRedirect: '/login' }));
 
-app.post('/auth/google', passport.authenticate('google', { scope: ['openid' ,'profile' ,'email'] }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['openid' ,'profile' ,'email'] }));
 
 
 app.get('/auth/google/callback', 
