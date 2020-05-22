4e3e32abca1022d31c6ce82d264a658bc506c1f2
Tanmay Awasekar <tanmayawasekar@gmail.com>
2020-05-22 10:48:36 +0530

    edit

--- fabfile.py
+++ fabfile.py
@@ -36,9 +36,9 @@ def _docker_compose_up():
     
     run(a)
     run(b)
-    run("~/.bashrc >> " + a)
-    run("~/.bashrc >> " + b)
-    run("source ~/.bashrc")
+    # run("~/.bashrc >> " + a)
+    # run("~/.bashrc >> " + b)
+    # run("source ~/.bashrc")
     run("SES_KEY=%s SES_PASSWORD=%s sudo docker-compose up --build -d" % (SES_KEY, SES_PASSWORD))
     # run("sudo docker system prune -y")
     # https://github.com/Ideonella-sakaiensis/lib_mysqludf_redis
